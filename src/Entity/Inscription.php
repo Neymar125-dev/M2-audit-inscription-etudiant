7c88462e7caaedf932d1a43243115b2bd2bb2c58
Neymar125-dev <angelopatrick560@gmail.com>
2025-02-24 07:52:24 +0100

    feat: mise en place admin crud

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InscriptionRepository::class)]
+class Inscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $matricule = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $droit = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->matricule;
+    }
+
+    public function setMatricule(string $matricule): static
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDroit(): ?int
+    {
+        return $this->droit;
+    }
+
+    public function setDroit(int $droit): static
+    {
+        $this->droit = $droit;
+
+        return $this;
+    }
+}
---
+++ src/Repository/AuditInscriptionRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\AuditInscription;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<AuditInscription>
+ *
+ * @method AuditInscription|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AuditInscription|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AuditInscription[]    findAll()
+ * @method AuditInscription[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AuditInscriptionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AuditInscription::class);
+    }
+
+//    /**
+//     * @return AuditInscription[] Returns an array of AuditInscription objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?AuditInscription
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
